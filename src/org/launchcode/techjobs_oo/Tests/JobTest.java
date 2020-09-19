7789290ce1a1223a69b442b3e4cc74a58ff0a58e
unknown <smkreter@gmail.com>
2020-09-18 20:54:53 -0500

    okay the problem is that I think I need to build all these things out seperately and make it so that they can be accessed in the test methods. that's what's not provided for so far! okeydoke.

--- src/org/launchcode/techjobs_oo/Tests/JobTest.java
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -3,28 +3,36 @@ package org.launchcode.techjobs_oo.Tests;
 import org.junit.Before;
 import org.junit.Test;
 import org.launchcode.techjobs_oo.*;
+import org.junit.Before;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
 public class JobTest {
 
-    @Before
-    Employer boeing = new Employer("Boeing");
-    Employer monsanto = new Employer("Bayer");
-    Location stLouis = new Location("St. Louis");
-    Location chicago = new Location("Chicago");
-    CoreCompetency busyWork = new CoreCompetency("Busy Work");
-    CoreCompetency projectManagement = new CoreCompetency("Project Management");
-    PositionType workerBee = new PositionType("Worker Bee");
-    PositionType projectManager = new PositionType("Project Manager");
-    Job testJob1 = new Job("Thing Doer II", boeing, stLouis, workerBee, busyWork);
-    Job testJob2 = new Job("Project Manager III", monsanto, chicago, projectManager, projectManagement);
-
     @Test
     public void emptyTest() {
         assertEquals(10, 10, .001);
     }
+
+    @Before
+    public void createTestObjects() {
+
+        Employer boeing = new Employer("Boeing");
+        Employer monsanto = new Employer("Bayer");
+        Location stLouis = new Location("St. Louis");
+        Location chicago = new Location("Chicago");
+        CoreCompetency busyWork = new CoreCompetency("Busy Work");
+        CoreCompetency projectManagement = new CoreCompetency("Project Management");
+        PositionType workerBee = new PositionType("Worker Bee");
+        PositionType projectManager = new PositionType("Project Manager");
+        Job testJob1 = new Job("Thing Doer II", boeing, stLouis, workerBee, busyWork);
+        Job testJob2 = new Job("Project Manager III", monsanto, chicago, projectManager, projectManagement);
+
+    }
+
+
+
 
     @Test
     public void testSettingJobId(){
