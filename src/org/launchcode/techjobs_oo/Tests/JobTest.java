b477a3fa8b65edbe14ce04824f6f38de3dfbf202
unknown <smkreter@gmail.com>
2020-09-19 12:42:33 -0500

    literally the answer was the next line of the instructions i'm sad

--- src/org/launchcode/techjobs_oo/Tests/JobTest.java
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -2,11 +2,14 @@ package org.launchcode.techjobs_oo.Tests;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.launchcode.techjobs_oo.*;
+//import org.launchcode.techjobs_oo.*;
+import org.launchcode.techjobs_oo.PositionType;
+import org.launchcode.techjobs_oo.Location;
+import org.launchcode.techjobs_oo.Employer;
+import org.launchcode.techjobs_oo.CoreCompetency;
+import org.launchcode.techjobs_oo.Job;
 
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.*;
 
 public class JobTest {
 
@@ -48,9 +51,29 @@ public class JobTest {
         CoreCompetency projectManagement = new CoreCompetency("Project Management");
         PositionType projectManager = new PositionType("Project Manager");
         Job secondTestJob = new Job("Project Manager III", monsanto, chicago, projectManager, projectManagement);
+        Job emptyConstructorTestJob = new Job();
         assertNotEquals(firstTestJob.getId(), null);
+        assertNotEquals(emptyConstructorTestJob.getId(), null);
         assertNotEquals(firstTestJob.getId(), secondTestJob.getId());
         assertEquals(firstTestJob.getId() + 1, secondTestJob.getId(), 0.01);
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields() {
+        Job productTester = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+//        assertEquals(productTester.getCoreCompetency().getClass(), CoreCompetency);
+//        assertEquals(productTester.getEmployer().getClass(), Employer);
+//        assertEquals(productTester.getLocation().getClass(), Location);
+//        assertEquals(productTester.getPositionType().getClass(), PositionType);
+        assertEquals(productTester.getName(), "Product tester");
+        assertEquals(productTester.getEmployer(), "ACME");
+//System.out.println(productTester.getName().getClass().toString());
+        assertEquals("class java.lang.String", productTester.getName().getClass().toString());
+//        assertTrue(productTester.getName().getClass() == Job.getName().getClass());
+//        assertEquals(productTester.getName().getClass(), Class String);
+//        assertTrue(productTester.getName().getClass().getName() == String);
+
+
     }
 
 
