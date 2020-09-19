3103892f28186c35f306c88750fd4f5ea73e267f
unknown <smkreter@gmail.com>
2020-09-19 10:45:26 -0500

    my set Job ID test is running okay so far hmm

--- src/org/launchcode/techjobs_oo/Tests/JobTest.java
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -26,22 +26,31 @@ public class JobTest {
 
     @Before
     public void createTestObject2() {
-        Employer monsanto = new Employer("Bayer");
-        Location chicago = new Location("Chicago");
-        CoreCompetency projectManagement = new CoreCompetency("Project Management");
-        PositionType projectManager = new PositionType("Project Manager");
-        Job testJob2 = new Job("Project Manager III", monsanto, chicago, projectManager, projectManagement);
+//        Employer monsanto = new Employer("Bayer");
+//        Location chicago = new Location("Chicago");
+//        CoreCompetency projectManagement = new CoreCompetency("Project Management");
+//        PositionType projectManager = new PositionType("Project Manager");
+//        Job testJob2 = new Job("Project Manager III", monsanto, chicago, projectManager, projectManagement);
 //        return testJob2;
     }
 
     @Test
     public void testSettingJobId(){
+        // createTestObject1
         Employer boeing = new Employer("Boeing");
         Location stLouis = new Location("St. Louis");
         CoreCompetency busyWork = new CoreCompetency("Busy Work");
         PositionType workerBee = new PositionType("Worker Bee");
         Job firstTestJob = new Job("Thing Doer II", boeing, stLouis, workerBee, busyWork);
+        // createTestObject2
+        Employer monsanto = new Employer("Bayer");
+        Location chicago = new Location("Chicago");
+        CoreCompetency projectManagement = new CoreCompetency("Project Management");
+        PositionType projectManager = new PositionType("Project Manager");
+        Job secondTestJob = new Job("Project Manager III", monsanto, chicago, projectManager, projectManagement);
         assertNotEquals(firstTestJob.getId(), null);
+        assertNotEquals(firstTestJob.getId(), secondTestJob.getId());
+        assertEquals(firstTestJob.getId() + 1, secondTestJob.getId(), 0.01);
     }
 
 
