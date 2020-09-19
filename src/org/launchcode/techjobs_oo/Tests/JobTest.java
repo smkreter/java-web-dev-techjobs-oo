a78559926c8e386db877db95b8232e3381bed9f3
unknown <smkreter@gmail.com>
2020-09-18 20:29:50 -0500

    i added junit testing and stuff

---
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -0,0 +1,16 @@
+package org.launchcode.techjobs_oo.Tests;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class JobTest {
+
+    @Test
+    public void emptyTest() {
+        assertEquals(10, 10, .001);
+    }
+
+
+
+
+}
